1540e63af382eafe2138556cb04b95b62683b21c
Ando Roots <ando@sqroot.eu>
2015-05-16 20:16:33 +0300

    Do a full rebuild on CI

--- _plugins/reading_time.rb
+++ _plugins/reading_time.rb
@@ -10,7 +10,7 @@ module ReadingTimeFilter
  
 		words = input.split.size;
 		minutes = ( words / words_per_minute ).floor
-		minutes_label = minutes === 1 ? " minute" : " minutes"
+		minutes_label = minutes === 1 ? "minute" : "minutes"
 		minutes > 0 ? "about #{minutes} #{minutes_label}" : "less than 1 minute"
 	end
 end
