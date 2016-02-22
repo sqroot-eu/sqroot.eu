8d9f1a298a627c91ddd1b39b2abd7360d7c71077
Ando Roots <ando@sqroot.eu>
2016-02-22 21:56:12 +0200

    Upgrade library versions in bower.json

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,4 +1,4 @@
 //=require jquery/dist/jquery.min
 //=require bootstrap/dist/js/bootstrap.min
 //=require clean-blog
-//=require picturefill/dist/picturefill
+//=require picturefill/dist/picturefill.min
